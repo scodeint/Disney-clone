1e4059fc03551df0f3b737123e7ecc2d6275a6d7
Oluwaseun Akinola <seunakinola082@gmail.com>
2023-02-17 23:17:49 +0100

    Returning the google login details into the browser for further actions.

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -6,17 +6,29 @@ import { GoogleAuthProvider,
          onAuthStateChanged 
         } from "firebase/auth";
 import { auth } from "../firebase";
+import { useEffect } from "react";
+import { useState } from "react";
 
 
-
+ 
  export const AuthContext = createContext();
 
-
 export const AuthContextProvider = ({children}) =>{
+    const [user, setUser] = useState({})
         const googleSignIn = () =>{
             const provider = new GoogleAuthProvider();
             signInWithPopup(auth, provider);
         };
+
+        useEffect (() =>{
+            const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+            setUser(currentUser);
+            console.log('User', currentUser);
+            });
+            return () =>{
+                unsubscribe();
+            }
+        }, []);
     return(
         <AuthContext.Provider value={{googleSignIn}}>
             {children}
