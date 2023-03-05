c85b9c6cac830707e12c6aad2015917fc4dd1735
Oluwaseun Akinola <seunakinola082@gmail.com>
2023-03-05 23:57:25 +0100

    Added the sign out functionalities that lands back at the homepage after clicking the sign out button.

--- src/App.js
+++ src/App.js
@@ -3,19 +3,20 @@ import Login from './componenets/Login';
 import Header from './componenets/Header';
 import './App.css';
 import { AuthContextProvider } from './context/AuthContext';
+import Home from './componenets/Home';
 
 
 function App() {
   return (
     <div className="App">
-      <AuthContextProvider>
+      
         <Router>
           <Header />
           <Routes>
             <Route path="/" element={<Login />} /> 
+            <Route path="/home" element={<Home />} />
           </Routes>
         </Router>
-      </AuthContextProvider>
     </div>
   );
 }
--- src/componenets/Header.js
+++ src/componenets/Header.js
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import styled from "styled-components";
 import { useDispatch, useSelector } from "react-redux";
 //import {useHistory} from "react-router-dom"
@@ -5,32 +6,50 @@ import { useNavigate } from "react-router-dom";
 //import { UserAuth , provider} from "../context/AuthContext";
 import { auth, provider } from "../firebase";
 import { selectUserName, 
-         userPhoto, 
-         selectLoginDetails, 
-         selectUserPhoto,
-         setUserLoginDetails,
+         selectUserPhoto, 
+         setUserLoginDetails, 
+         setSignOutState,
 } from "../features/user/userSlice";
+import { async } from "@firebase/util";
 
 
 
 
 const Header = (props) =>{
     const dispatch = useDispatch();
-    const navigate  =  useNavigate();
+    const history  =  useNavigate();
     const userName = useSelector(selectUserName);
     const userPhoto = useSelector(selectUserPhoto);
     
-   // const {googleSignIn} = UserAuth();
+useEffect(() =>{
+    auth.onAuthStateChanged(async(user) =>{
+        if(user){
+            setUser(user)
+            history.push("/home")
+        }
+    })
+}, [userName]);
+
     const handleAuth = () =>{
+        if(!userName){
         auth
         .signInWithPopup(provider)
         .then((result) =>{
             setUser(result.user)
         })
       .catch ((error) =>{
-        console.log(error.message);
+        alert(error.message);
        });
-    };
+    } else if (userName){
+        auth
+        .signOut()
+        .then(() =>{
+            dispatch(setSignOutState());
+            history.push("/");
+        })
+        .catch((err) => alert(err.message));
+    }
+};
 
     const setUser = (user) =>{
         dispatch(
@@ -77,7 +96,13 @@ const Header = (props) =>{
                 <span>HOME</span>
             </a>
         </NavMenu>
-        <UserImg src={userPhoto} alt={userName} />
+        <SignOut>
+            <UserImg src={userPhoto} alt={userName} />
+            <DropDown>
+                <span onClick={handleAuth}>Sign out</span>
+            </DropDown>
+        </SignOut>
+      
         </>
     )}
     </Nav>
@@ -192,6 +217,43 @@ const Login = styled.a`
 
 const UserImg = styled.img`
     height: 100%;
+`;
+
+const DropDown = styled.div`
+    position: absolute;
+    top: 48px;
+    right: 0px;
+    background: rgb(19, 19, 19);
+    border: 1px solid rgba(151, 151, 151, 0.34);
+    border-radius: 4px;
+    box-shadow: rgb(0 0 0 /50%) 0px 0px 10px 0px;
+    padding: 10px;
+    font-size: 14px;
+    letter-spacing: 3px;
+    width: 100px;
+    opacity: 0;
+`;
+const SignOut = styled.div`
+    position: relative;
+    height: 48px;
+    width: 48px;
+    display: flex;
+    cursor: pointer;
+    align-items: center;
+    justify-content: center;
+
+    ${UserImg} {
+        border-radius: 50%;
+        width: 100%
+        height: 100%;
+    }
+
+    &:hover{
+        ${DropDown} {
+            opacity: 1;
+            transition-duration: 1s;
+        }
+    }
 `;
 
 
